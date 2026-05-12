d775bb1135667c825208dd6f1ae0b5216ff1b70f
Rahul Balajee <rahrkb4@gmail.com>
2026-05-12 15:19:57 +0530

    update routes

--- src/app.py
+++ src/app.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 @app.route('/api/v1/details')
 def details():
     return jsonify({
-        'name': 'John Doe',
-        'age': 30,
-        'email': 'john.doe@example.com'
+        'message': 'Hello World'
+    }), 200
+
+@app.route('/api/v1/healthz')
+def healthz():
+    return jsonify({
+        'message': 'OK'
     }), 200
 
 if __name__ == '__main__':
