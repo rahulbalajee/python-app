cf758344e6d7455c3a73f1ba432a713d52943f1e
Rahul Balajee <rahrkb4@gmail.com>
2026-05-12 15:33:19 +0530

    bump

--- src/app.py
+++ src/app.py
@@ -1,17 +1,20 @@
 from flask import Flask, jsonify
+import socket
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/api/v1/details')
 def details():
     return jsonify({
-        'message': 'Hello World'
+        'time': datetime.now().isoformat(),
+        'hostname': socket.gethostname()
     }), 200
 
 @app.route('/api/v1/healthz')
 def healthz():
     return jsonify({
-        'message': 'OK'
+        'status': 'healthy'
     }), 200
 
 if __name__ == '__main__':
